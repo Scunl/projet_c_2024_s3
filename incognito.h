282d7b74c13ac1e7d904f5833e0fb8c0645f2c20
Scunl <walid.amara.pro@gmail.com>
2024-10-17 00:44:47 +0200

    Fix du main

--- incognito.h
+++ incognito.h
@@ -1,4 +1,6 @@
-typedef enum _couleur {BLANC, NOIR} Couleur;
+#define TAILLE 5
+
+typedef enum _couleur {VIDE, BLANC, NOIR} Couleur;
 typedef enum _type{CHEVALIER, ESPION} Type;
 
 typedef struct _pion {
@@ -6,7 +8,7 @@ typedef struct _pion {
     Couleur couleur;
 } Pion;
 
-typedef struct {
+typedef struct _vecteur {
     int x, y;
 } Case, Direction;
 
--- main.c
+++ main.c
@@ -1,19 +1,56 @@
+#include "incognito.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <incognito.h>
-
-#define TAILLE 5 // Taille du plateau
-
 
 int nb_pion(int tplateau) {
     if (tplateau <= 8)
-        return ((tplateau -1)(tplateau - 2)/2.) - 1;
-    else
-        return -1;
+        return ((tplateau - 1) * (tplateau - 2) / 2) - 1;
+    return -1;
 }
 
+void init_plateau(int tplateau, Jeu *jeu) {
+    int tmp = 2;
+    int tmp2 = -2;
 
+    for (int i = 0; i < tplateau; i++) {
+        for (int j = 0; j < tplateau; j++) {
+            jeu->plateau[i][j].couleur = 0;
+            jeu->plateau[i][j].type = 0;
+        }
+    }
+
+    for (int i = 0; i < tplateau; i++) {
+        for (int j = 0; j < tplateau; j++) {
+            if (i < tplateau - 2 && j >= tmp) {
+                if (i != 0 || j != tplateau - 1)
+                    jeu->plateau[i][j].couleur = NOIR;
+                else
+                    jeu->plateau[i][j].couleur = 8;
+            }
+            if (i >= 2 && j <= tmp2) {
+                if (i != tplateau - 1 || j != 0)
+                    jeu->plateau[i][j].couleur = BLANC;
+                else
+                    jeu->plateau[i][j].couleur = 9;
+            }
+        }
+        tmp++;
+        tmp2++;
+    }
+}
+
+void affichage(int tplateau, const Jeu *jeu) {
+    for (int i = 0; i < tplateau; i++) {
+        for (int j = 0; j < tplateau; j++) {
+            printf("%d ", jeu->plateau[i][j].couleur);
+        }
+        printf("\n");
+    }
+}
 
 int main(void) {
+    Jeu jeu;
+    init_plateau(TAILLE, &jeu);
+    affichage(TAILLE, &jeu);
     return 0;
 }
